b0f659049532352f003358543650b2a1a27a7566
Xavier Escamilla <xavier.escamillap@gmail.com>
2020-10-22 00:06:05 -0500

    Conexión a DB MySQL

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const pokemon = express.Router();
-const pk = require('../pokedex.json').pokemon;
+const db = require('../config/database');
 
 pokemon.post("/", (req, res, next) => {
-    return res.status(200).send(req.body);
+    return res.status(200).json(req.body);
 });
 
-pokemon.get("/", (req, res, next) => {
-    return res.status(200).send(pk);
+pokemon.get("/", async (req, res, next) => {
+    const pkmn = await db.query("SELECT * FROM pokemon");
+    return res.status(200).json(pkmn);
 });
 
-pokemon.get("/:id([0-9]{1,3})", (req, res, next) => {
-    const id = req.params.id - 1;
+pokemon.get("/:id([0-9]{1,3})", async (req, res, next) => {
+    const id = req.params.id;
     if (id >= 0 && id <= 150) {
-        return res.status(200).send(pk[req.params.id-1]);
+        const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id="+id);
+        return res.status(200).json(pkmn);
     }
     return res.status(404).send("Pokemon no encontrado");
 });
 
-pokemon.get('/:name([A-Za-z]+)', (req, res, next) => {
+pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const name = req.params.name;
-    const pkmn = pk.filter((p) => {
-        return (p.name.toUpperCase() == name.toUpperCase()) && p;
-    });
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name="+name);
     if (pkmn.length > 0) {
-        res.status(200).send(pkmn);
+        return res.status(200).json(pkmn);
     }
     return res.status(404).send("Pokemon no encontrado");
 });
